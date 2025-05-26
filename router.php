512d053abba86a22724878474ae70f15981bf2b7
nathaniel1219 <nathanielshalin@gmail.com>
2025-05-26 12:30:20 +0530

    Router connection update

--- router.php
+++ router.php
@@ -1,15 +1,12 @@
 <?php
-
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
-if($path === "/Arcanum/home"){
+if($path === "/Arcanum/index.php"){
     echo "Home Page";
 }
-else if($path === "/Arcanum/cards"){
-    echo "<h1>This is the cards page</h1>";
+else if($path === "/Arcanum/pokemon.php"){
+    echo "<h1>This is the pokemon page</h1>";
 }
 else{
-    echo "fugoff";
+    echo "error 404";
 }
-
 ?>
